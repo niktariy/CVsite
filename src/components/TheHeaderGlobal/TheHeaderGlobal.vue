2e9ba99389036ef41d794283a5eca6c80981230e
Veronika <vervaspro@icloud.com>
2018-08-10 17:14:46 +0300

    add animated open mobile menu, add fixed header component

--- src/components/TheHeaderGlobal/TheHeaderGlobal.vue
+++ src/components/TheHeaderGlobal/TheHeaderGlobal.vue
@@ -1,49 +1,59 @@
 <template>
-
-  <header class="header" id="header">
-    <div class="header__wrapper grid">
-      <router-link to="/" class="header__logo" exact>
-        <img
-          :src="siteLogo"
-          height="56"
-          width="auto"
-          alt="Veronika"
-          class="logo"
-        />
-      </router-link>
-      <div class="section section--tab">
-        <div class="burger" @click="openMobileMenu()">
-            <div class="burger__buns">
-                <span class=" burger__bun bun bun--top"></span>
-                <span class=" burger__bun bun bun--mid"></span>
-                <span class=" burger__bun bun bun--bot"></span>
+  <fixed-header :fixed.sync="isFixed">
+    <header
+      class="header" id="header"
+      :class="{ 'header--smaller': isFixed }"
+      :threshold="300"
+    >
+      <div class="header__wrapper grid">
+        <router-link to="/" class="header__logo" exact>
+          <img
+            :src="siteLogo"
+            height="56"
+            width="auto"
+            alt="Veronika"
+            class="logo"
+          />
+        </router-link>
+        <div class="section section--tab">
+          <div class="burger" @click="openMobileMenu()">
+              <div class="burger__buns">
+                  <span class=" burger__bun bun bun--top"></span>
+                  <span class=" burger__bun bun bun--mid"></span>
+                  <span class=" burger__bun bun bun--bot"></span>
+            </div>
           </div>
+          <nav class="tab tab--valentine">
+            <ul class="tab__list" id="header-nav">
+              <router-link
+                tag="li"
+                class="tab__item"
+                v-for="item in items"
+                :to="item.linkTo"
+                :key="item.id"
+              > {{ item.name }}
+              </router-link>
+              <li class="tab__line"><span></span></li>
+            </ul>
+          </nav>
         </div>
-        <nav class="tab tab--valentine">
-          <ul class="tab__list" id="header-nav">
-            <router-link
-              tag="li"
-              class="tab__item"
-              v-for="item in items"
-              :to="item.linkTo"
-              :key="item.id"
-            > {{ item.name }}
-            </router-link>
-            <li class="tab__line"><span></span></li>
-          </ul>
-        </nav>
       </div>
-    </div>
-  </header>
+    </header>
+  </fixed-header>
 </template>
 
 <script>
+import FixedHeader from 'vue-fixed-header'
 
 export default {
   name: 'l-header',
   props: ['className'],
+  components: {
+    FixedHeader
+  },
   data() {
     return {
+      isFixed: false,
       siteLogo: "/img/LetterV.svg",
       items: [
         { name: 'About',
@@ -93,7 +103,7 @@ export default {
           this.menuOpened = true;
           return;
         }
-        tabListClasses.remove('animated','slideInRight')
+        tabListClasses.remove('slideInRight')
         tabListClasses.add('slideOutRight');
         this.menuOpened = false;
     },
